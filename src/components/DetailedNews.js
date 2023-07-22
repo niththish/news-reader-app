4029386e74d053df731bf53989f0a0c6138175f1
niththish <niththish21@gmail.com>
2023-07-22 15:21:24 +0530

    modified detailed news page ui

--- src/components/DetailedNews.js
+++ src/components/DetailedNews.js
@@ -12,20 +12,20 @@ const DetailedNews = () => {
     <div className="detailed-news">
       <h4>{data.title}</h4>
       <div className="author-info">
-        <p>
-          Author:- <span>{data.author}</span>
-        </p>
-        <p>
-          Published at:-{" "}
-          <span>{new Date(data.publishedAt).toDateString()}</span>
-        </p>
+        <p>{data.author}</p>
+        <p className="date">{new Date(data.publishedAt).toDateString()}</p>
       </div>
       <div className="detailed-news-image">
         <img src={data.urlToImage} />
       </div>
       <div className="detailed-news-content">
-        <p>{data.description}</p>
         <p>{data.content}</p>
+        <p>
+          source:-{" "}
+          <span style={{ color: "#C100A2", fontWeight: "bolder" }}>
+            {data.source.name}
+          </span>
+        </p>
         <a href={data.url} target="_blank">
           click here to read more
         </a>
